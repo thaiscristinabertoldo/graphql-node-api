bf0854d77880aca3736a5e152f430665e78d2692
thais.bertoldo <thais.bertoldo@softplan.com.br>
2021-09-04 12:27:26 -0300

    feat: adiciona config subscription

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,69 @@
+require('dotenv').config();
+
 import 'reflect-metadata';
 import './utils/connection';
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+
+import { ApolloServer } from 'apollo-server-express';
+import { execute, subscribe } from 'graphql';
 import { buildSchema } from 'type-graphql';
-import { ApolloServer } from 'apollo-server';
-import CategoryResolver from './graphql/category/CategoryResolver';
-import VideoResolver from './graphql/video/VideoResolver';
+import { SubscriptionServer } from 'subscriptions-transport-ws';
+
+import { CategoryResolver } from './graphql/category';
+import { MovieResolver } from './graphql/movie';
+import { MovieNotificationResolver } from './graphql/subscription/Movie';
 
 async function bootstrap() {
+  const app = express();
+
+  const httpServer = http.createServer(app);
+
+  app.use(cors({ origin: '*' }));
+
   const schema = await buildSchema({
-    resolvers: [CategoryResolver, VideoResolver],
+    resolvers: [CategoryResolver, MovieResolver, MovieNotificationResolver],
   });
 
-  const server = new ApolloServer({ schema });
+  const server = new ApolloServer({
+    schema,
+    plugins: [
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              subscriptionServer.close();
+            },
+          };
+        },
+      },
+    ],
+  });
+  await server.start();
 
-  server.listen({ port: 4100 }, () => console.log('Running'));
+  const subscriptionServer = SubscriptionServer.create(
+    {
+      schema,
+      execute,
+      subscribe,
+    },
+    {
+      server: httpServer,
+      path: server.graphqlPath,
+    }
+  );
+
+  server.applyMiddleware({
+    app,
+    cors: false,
+  });
+
+  httpServer.listen(process.env.PORT, () =>
+    console.log(
+      `Server is now running on http://localhost:${process.env.PORT}/graphql`
+    )
+  );
 }
 
 bootstrap();
